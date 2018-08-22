93ba9f5225898083011e7ac3fb13e4c27ba1e355
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-08-21 22:32:55 -0300

    Configurando testes de integração na API HTTP

--- src/test/java/br/com/receita/ReceitaApplicationTests.java
+++ src/test/java/br/com/receita/ReceitaApplicationTests.java
@@ -2,15 +2,22 @@ package br.com.receita;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
 public class ReceitaApplicationTests {
-
+	
+	@Value("${local.server.port}")
+	protected int porta;
 	@Test
 	public void contextLoads() {
+		System.out.println("Porta : " + porta);
 	}
 
 }
