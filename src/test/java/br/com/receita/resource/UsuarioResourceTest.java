9fca8a936fbbc6db56d3266217b53d3af90e2875
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-08-25 00:01:12 -0300

    Tdd de salvar usuario

--- src/test/java/br/com/receita/resource/UsuarioResourceTest.java
+++ src/test/java/br/com/receita/resource/UsuarioResourceTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
 import br.com.receita.ReceitaApplicationTests;
+import br.com.receita.domain.Usuario;
+import io.restassured.http.ContentType;
 
 public class UsuarioResourceTest extends ReceitaApplicationTests{
 
@@ -25,6 +27,29 @@ public class UsuarioResourceTest extends ReceitaApplicationTests{
 					"email", equalTo("crane@gmail.com")
 					);
 			
+	}
+	
+	@Test
+	public void salvarUsuario () throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Manuel L.");
+		usuario.setEmail("manuell@gmail.com");
+		usuario.setSenha("123");
+		
+		given()
+			.request()
+			.header("Accept", ContentType.ANY)
+			.header("content-type", ContentType.JSON)
+			.body(usuario)
+		.when()
+		.post("/api/v1/usuarios/")
+		.then()	
+				.log().headers()
+			.and()
+				.log().body()
+			.and()
+				.statusCode(HttpStatus.CREATED.value())
+				.header("Location", equalTo("http://localhost:"+porta+"/api/v1/usuarios/2"));			
 	}
 
 }
