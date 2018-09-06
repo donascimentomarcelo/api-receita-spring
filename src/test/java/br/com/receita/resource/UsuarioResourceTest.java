945c2dd1443f4875f6951ef3e56b68990b8c84be
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-09-05 22:55:11 -0300

    Criando teste para inserir usuario c email duplicado

--- src/test/java/br/com/receita/resource/UsuarioResourceTest.java
+++ src/test/java/br/com/receita/resource/UsuarioResourceTest.java
@@ -5,11 +5,15 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
 
 import br.com.receita.ReceitaApplicationTests;
 import br.com.receita.domain.Usuario;
 import io.restassured.http.ContentType;
 
+@Sql(value = "/limpar-dados.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/insere-dados.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/limpar-dados.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UsuarioResourceTest extends ReceitaApplicationTests{
 
 	@Test
@@ -62,6 +66,31 @@ public class UsuarioResourceTest extends ReceitaApplicationTests{
 			.and()
 				.statusCode(HttpStatus.CREATED.value())
 				.header("Location", equalTo("http://localhost:"+porta+"/api/v1/usuarios/2"));
+			
+	}
+	
+	@Test
+	public void salvar_usuario_com_email_duplicado () throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Crane 2");
+		usuario.setEmail("crane@gmail.com");
+		usuario.setSenha("123");
+		
+		given()
+			.request()
+			.header("Accept", ContentType.ANY)
+			.header("content-type", ContentType.JSON)
+			.body(usuario)
+		.when()
+		.post("/api/v1/usuarios/")
+		.then()	
+				.log().headers()
+			.and()
+				.log().body()
+			.and()
+				.statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value())
+				.and()
+				.body("msg", equalTo("O e-mail crane@gmail.com já está sendo usado."));
 			
 	}
 
--- src/test/resources/limpar-dados.sql
+++ src/test/resources/limpar-dados.sql
@@ -1,2 +1,5 @@
 delete from endereco;
 delete from usuario;
+
+ALTER TABLE endereco AUTO_INCREMENT = 1;
+ALTER TABLE usuario AUTO_INCREMENT = 1;
