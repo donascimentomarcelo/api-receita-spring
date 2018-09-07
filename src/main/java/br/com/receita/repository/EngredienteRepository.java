ea8e9cffedd82d8c8b32aa87ba201676edc05f70
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-09-07 18:52:00 -0300

    Revert "Criando funcionalidade de filtro para engrediente"
    This reverts commit 3c340b9bd9f6bc04138ee17199e997b731cd240e.

--- src/main/java/br/com/receita/repository/EngredienteRepository.java
+++ src/main/java/br/com/receita/repository/EngredienteRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.receita.domain.Engrediente;
-import br.com.receita.repository.helper.EngredienteQuerieRepository;
 
 @Repository
-public interface EngredienteRepository extends JpaRepository<Engrediente, Integer>, EngredienteQuerieRepository {
+public interface EngredienteRepository extends JpaRepository<Engrediente, Integer> {
 
 	List<Engrediente> findByDescricaoIgnoreCase(String descricao);
 
--- src/main/java/br/com/receita/service/EngredienteService.java
+++ src/main/java/br/com/receita/service/EngredienteService.java
@@ -1,18 +1,9 @@
 package br.com.receita.service;
 
-import java.util.List;
-
 import br.com.receita.domain.Engrediente;
-import br.com.receita.repository.filtro.EngredienteFiltro;
 
 public interface EngredienteService {
 
 	Engrediente salvar(Engrediente engrediente);
-
-	/**
-	 * @param filtro
-	 * @return
-	 */
-	List<Engrediente> filtrar(EngredienteFiltro filtro);
 
 }
