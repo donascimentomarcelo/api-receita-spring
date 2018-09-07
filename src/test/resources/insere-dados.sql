7e16f4c2a8d53e1b848d5843a362c4cda4a66846
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-09-07 18:50:09 -0300

    Criando massa de dados para engrediente

--- src/test/resources/insere-dados.sql
+++ src/test/resources/insere-dados.sql
@@ -1,3 +1,17 @@
-INSERT INTO `usuario` (`id`, `email`, `nome`, `senha`) VALUES ('1', 'crane@gmail.com', 'Crane', '123');
+INSERT INTO `usuario` (`id`, `email`, `nome`, `senha`) 
+	VALUES 
+	('1', 'crane@gmail.com', 'Crane', '123');
 
-INSERT INTO `endereco` (`id`, `bairro`, `cep`, `complemento`, `localidade`, `logradouro`, `uf`, `usuario_id`) VALUES ('1', 'Braz de pina', '21012409', 'Rua Trinta e Seis - Braz de Pina', 'Quadra C9', NULL, 'RJ', '1')
+INSERT INTO `endereco` (`id`, `bairro`, `cep`, `complemento`, `localidade`, `logradouro`, `uf`, `usuario_id`) 
+	VALUES 
+	('1', 'Braz de pina', '21012409', 'Rua Trinta e Seis - Braz de Pina', 'Quadra C9', NULL, 'RJ', '1');
+
+INSERT INTO `engrediente` (`id`, `descricao`, `medida`, `quantidade`, `id_receita`) 
+	VALUES 
+	('1', 'Leite moça', '3', '2', '5');
+INSERT INTO `engrediente` (`id`, `descricao`, `medida`, `quantidade`, `id_receita`) 
+	VALUES	
+	('2', 'Fubá', '4', '3', '6');
+INSERT INTO `engrediente` (`id`, `descricao`, `medida`, `quantidade`, `id_receita`) 
+	VALUES	
+	('3', 'Leite', '2', '8', '7');
--- src/test/resources/limpar-dados.sql
+++ src/test/resources/limpar-dados.sql
@@ -1,5 +1,7 @@
 delete from endereco;
 delete from usuario;
+delete from engrediente;
 
 ALTER TABLE endereco AUTO_INCREMENT = 1;
 ALTER TABLE usuario AUTO_INCREMENT = 1;
+ALTER TABLE engrediente AUTO_INCREMENT = 1;
