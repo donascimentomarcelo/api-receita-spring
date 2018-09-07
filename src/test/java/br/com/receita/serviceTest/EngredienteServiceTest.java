361e35a2eac13cbe4812958362aee19f5659103e
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-09-07 18:50:57 -0300

    Criando testes para engrediente service

--- src/test/java/br/com/receita/serviceTest/EngredienteServiceTest.java
+++ src/test/java/br/com/receita/serviceTest/EngredienteServiceTest.java
@@ -10,12 +10,14 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.receita.domain.Engrediente;
 import br.com.receita.domain.enums.Medida;
 import br.com.receita.repository.EngredienteRepository;
+import br.com.receita.repository.filtro.EngredienteFiltro;
 import br.com.receita.service.EngredienteService;
 import br.com.receita.service.impl.EngredienteServiceImpl;
 
@@ -24,14 +26,17 @@ public class EngredienteServiceTest {
 	
 	private static final String DESCRICAO = "Leite";
 	private static final Integer QUANTIDADE = 1;
+	private static final Medida MEDIDA = Medida.LITROS;
 
 	
 	@MockBean
 	private EngredienteRepository engredienteRepository;
 	private EngredienteService engredienteService;
-	
+	@Mock
 	private Engrediente engrediente;
 	private List<Engrediente> listaDeEngredientes;
+	@Mock
+	private EngredienteFiltro filtro;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -42,23 +47,114 @@ public class EngredienteServiceTest {
 		engrediente.setMedida(Medida.LITROS);
 		engrediente.setQuantidade(QUANTIDADE);
 		
+		filtro = new EngredienteFiltro();
+		filtro.setDescricao(DESCRICAO);
+		
 		listaDeEngredientes = new ArrayList<Engrediente>();
 		listaDeEngredientes.add(0, engrediente);
 		
 		when(engredienteRepository.findByDescricaoIgnoreCase(DESCRICAO)).thenReturn(listaDeEngredientes);
+		when(engredienteService.filtrar(filtro)).thenReturn(listaDeEngredientes);
 	}
 	
 	@Test
-	public void salvarEngredienteNoRepositorio() throws Exception {
+	public void deve_salvar_engrediente_no_repositorio() throws Exception {
 		engredienteService.salvar(engrediente);
 		verify(engredienteRepository).save(engrediente);
 	}
 	
 	@Test
-	public void pesquisarPorNomeOuMedida() throws Exception {
+	public void deve_pesquisar_por_descricao() throws Exception {
 		when(engredienteRepository.findByDescricaoIgnoreCase(DESCRICAO)).thenReturn(listaDeEngredientes);
 		List<Engrediente> lista = engredienteRepository.findByDescricaoIgnoreCase(DESCRICAO);
 		assertThat(lista.get(0).getDescricao()).isEqualTo(DESCRICAO);
 	}
+	
+	@Test
+	public void deve_pesquisar_engrediente_por_descricao_usando_filtro() throws Exception {
+		//cenario
+		filtro = new EngredienteFiltro();
+		filtro.setDescricao(DESCRICAO);
+		
+		//acao
+		when(engredienteService.filtrar(filtro)).thenReturn(listaDeEngredientes);
+		
+		//verificacao
+		List<Engrediente> list = engredienteService.filtrar(filtro);
+		assertThat(list.get(0).getDescricao()).isEqualTo(DESCRICAO);
+		
+	}
+	
+	/**
+	 * 
+	 * @Author Marcelo Nascimento
+	 * @Date 7 de set de 2018
+	 * @Project receita
+	 * @Package br.com.receita.serviceTest
+	 * @Desc Testa deve retornar um engrediente filtrado por descricao e quantidade
+	 */
+	@Test
+	public void deve_pesquisar_engrediente_por_descricao_e_quantidade_usando_filtro() throws Exception{
+		//cenario
+		filtro = new EngredienteFiltro();
+		filtro.setDescricao(DESCRICAO);
+		filtro.setQuantidade(QUANTIDADE);
+		
+		//acao
+		when(engredienteService.filtrar(filtro)).thenReturn(listaDeEngredientes);
+		
+		//verificacao
+		List<Engrediente> list = engredienteService.filtrar(filtro);
+		assertThat(list.get(0).getDescricao()).isEqualTo(DESCRICAO);
+		assertThat(list.get(0).getQuantidade()).isEqualTo(QUANTIDADE);
+	}
+	
+	/**
+	 * 
+	 * @Author Marcelo Nascimento
+	 * @Date 7 de set de 2018
+	 * @Project receita
+	 * @Package br.com.receita.serviceTest
+	 * @Desc Testa deve retornar um engrediente filtrado por quantidade
+	 */
+	@Test
+	public void deve_pesquisar_engrediente_por_quantidade_usando_filtro() {
+		//cenario
+		filtro = new EngredienteFiltro();
+		filtro.setQuantidade(QUANTIDADE);
+		
+		//acao
+		when(engredienteService.filtrar(filtro)).thenReturn(listaDeEngredientes);
+		
+		//verificacao
+		List<Engrediente> list = engredienteService.filtrar(filtro);
+		assertThat(list.get(0).getDescricao()).isEqualTo(DESCRICAO);
+		assertThat(list.get(0).getQuantidade()).isEqualTo(QUANTIDADE);
 
+	}
+	
+	/**
+	 * 
+	 * @Author Marcelo Nascimento
+	 * @Date 7 de set de 2018
+	 * @Project receita
+	 * @Package br.com.receita.serviceTest
+	 * @Desc Testa deve retornar um engrediente filtrado por descricao e quantidade 
+	 */
+	@Test
+	public void deve_pesquisar_engrediente_por_medida() throws Exception {
+		//cenario
+		filtro = new EngredienteFiltro();
+		filtro.setMedida(MEDIDA);
+		
+		//acao
+		when(engredienteService.filtrar(filtro)).thenReturn(listaDeEngredientes);
+		
+		//verificacao
+		List<Engrediente> list = engredienteService.filtrar(filtro);
+		assertThat(list.get(0).getDescricao()).isEqualTo(DESCRICAO);
+		assertThat(list.get(0).getQuantidade()).isEqualTo(QUANTIDADE);
+		assertThat(list.get(0).getMedida()).isEqualTo(MEDIDA);
+
+	}
 }
