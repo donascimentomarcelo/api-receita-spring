076461948d321591eef0d1b9c3b456bc2a0108d4
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-09-26 21:52:54 -0300

    Criando metodo para salvar receita

--- src/main/java/br/com/receita/resource/ReceitaResource.java
+++ src/main/java/br/com/receita/resource/ReceitaResource.java
@@ -1,12 +1,16 @@
 package br.com.receita.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.receita.domain.Receita;
 import br.com.receita.service.ReceitaService;
@@ -22,5 +26,14 @@ public class ReceitaResource {
 	ResponseEntity<List<Receita>> listar() {
 		List<Receita> lista = receitaService.listar();
 		return ResponseEntity.ok().body(lista);
+	}
+	
+	@PostMapping
+	ResponseEntity<?> salvar(@RequestBody Receita receita) {
+		receita = receitaService.salvar(receita);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(receita.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
