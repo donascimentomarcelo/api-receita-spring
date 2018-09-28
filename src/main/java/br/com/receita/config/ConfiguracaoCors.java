c57d5e1649ddcbe8f4dc8acbdff61a842388100a
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-09-27 22:44:37 -0300

    Configurando cors

---
+++ src/main/java/br/com/receita/config/ConfiguracaoCors.java
@@ -0,0 +1,33 @@
+package br.com.receita.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Author Marcelo Nascimento
+ * @Date 27 de set de 2018
+ * @Project receita
+ * @Package br.com.receita.config
+ * @Desc Classe que realiza a configuracao do cors do sistema.
+ */
+
+@Configuration
+public class ConfiguracaoCors {
+	
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
