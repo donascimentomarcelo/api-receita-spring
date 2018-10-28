cc358dee7570105625973665a8f780b069c55d98
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-10-28 11:41:45 -0300

    Correcao no controlador de comentarios.

--- src/main/java/br/com/receita/resource/ComentariosResource.java
+++ src/main/java/br/com/receita/resource/ComentariosResource.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.receita.domain.Avaliacao;
 import br.com.receita.domain.Comentario;
 import br.com.receita.service.ComentarioService;
 
@@ -35,5 +37,11 @@ public class ComentariosResource {
 	ResponseEntity<Comentario> criar(@RequestBody Comentario comentario, @PathVariable Integer avaliacao_id) {
 		Comentario retorno = comentarioService.salvar(comentario, avaliacao_id);
 		return ResponseEntity.ok(retorno);
+	}
+	
+	@PutMapping
+	ResponseEntity<?> atualizar(@RequestBody Comentario comentario, @PathVariable Integer id) {
+		comentarioService.atualizar(comentario, id);
+		return ResponseEntity.noContent().build();
 	}
 }
